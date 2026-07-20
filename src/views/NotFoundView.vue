bc8019299efe944a883a7f3b33131c2925208e3c
Bang Xiao <bangxiao0927@gmail.com>
2026-07-19 21:26:34 -0700

    feat: add route not found view

---
+++ src/views/NotFoundView.vue
@@ -0,0 +1,8 @@
+<template>
+  <section class="route-message" aria-labelledby="not-found-title">
+    <p class="eyebrow">404 error</p>
+    <h1 id="not-found-title">We could not find that page.</h1>
+    <p>Check the address or return to the HSclubs Guide home page.</p>
+    <RouterLink class="text-link" to="/"> Return home </RouterLink>
+  </section>
+</template>
