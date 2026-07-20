2e74c79945c15d867175c4cae94f46de742a6fa3
Bang Xiao <bangxiao0927@gmail.com>
2026-07-19 21:26:34 -0700

    feat: add accessible site header

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,12 @@
+<template>
+  <header class="site-header">
+    <a class="skip-link" href="#main-content">Skip to main content</a>
+    <div class="header-inner">
+      <RouterLink class="brand" to="/" aria-label="HSclubs Guide home">
+        <span class="brand-mark" aria-hidden="true">H</span>
+        <span>HSclubs Guide</span>
+      </RouterLink>
+      <span class="header-tagline">Verified school directories</span>
+    </div>
+  </header>
+</template>
