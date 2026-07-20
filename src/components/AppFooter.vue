85da1c0203fed365be6f05a199cd20927f285dab
Bang Xiao <bangxiao0927@gmail.com>
2026-07-19 21:26:34 -0700

    feat: add product boundary footer

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,8 @@
+<template>
+  <footer class="site-footer">
+    <div class="footer-inner">
+      <p>HSclubs Guide helps students find independent, school-owned club directories.</p>
+      <p>No student accounts or membership data are collected here.</p>
+    </div>
+  </footer>
+</template>
